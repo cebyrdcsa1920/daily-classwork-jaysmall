b4dac42d8c45ef5ba96a52e035b132144456f1a6
jaysmall <jayronsmall16@yahoo.com>
2019-09-05 14:18:39 +0000

    Make cleats class

---
+++ week4/Cleats.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+public class Cleats
+{
+    private String brand;
+    private Color color
+    private double size
+    public Cleats()
+    {
+        brand = "Adidas;"
+        color = Color. PURPLE;
+        size = 10;
+    }
+
+    public String toString()
+    {
+        return "This is my Adidas";
+    }
+    }
